3bff9bfa42a95235c91493ea7d44c71d2e456580
CAGE <cageunknowner@googlemail.com>
2011-07-01 13:14:31 +0200

    CHG: Generals updating only on page loading, reducing calls

--- js/castleage.js
+++ js/castleage.js
@@ -9,11 +9,10 @@ var CastleAge = {
 
 com.init(com.port.castleAge);
 console.log('CA started.');
-com.send(com.task.castleAgeReady, com.port.facebook, null);
 
 tools['Page'].cache['allPages']();
 if (tools['Page'].cache[$('#current_pg_info').attr('value') + '.php']) {
-	tools['Page'].cache[$('#cagePageURL').attr('value') + '.php']();
+	tools['Page'].cache[$('#current_pg_info').attr('value') + '.php']();
 }
 
 $('head').append(
@@ -21,10 +20,15 @@ $('head').append(
 				+ chrome.extension.getURL('css/trontastic/jquery-ui.css')
 				+ '">');
 
-$('#globalContainer').live('DOMSubtreeModified', function() {// DOMSubtreeModified
-
-	general.update();
-
-});
+/*
+ * $('#globalContainer').live('DOMSubtreeModified', function() {//
+ * DOMSubtreeModified
+ * 
+ * general.update();
+ * 
+ * });
+ */
 
 initTools();
+
+com.send(com.task.castleAgeReady, com.port.facebook, null);
