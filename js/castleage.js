8d3481de570e3519aa6ce76f5a69d57cbae9b836
CAGE <cageunknowner@googlemail.com>
2011-07-01 11:19:25 +0200

    ADD: Pagemod on startup

--- js/castleage.js
+++ js/castleage.js
@@ -11,9 +11,15 @@ com.init(com.port.castleAge);
 console.log('CA started.');
 com.send(com.task.castleAgeReady, com.port.facebook, null);
 
+tools['Page'].cache['allPages']();
+if (tools['Page'].cache[$('#current_pg_info').attr('value') + '.php']) {
+	tools['Page'].cache[$('#cagePageURL').attr('value') + '.php']();
+}
+
 $('head').append(
 		'<link rel="stylesheet" type="text/css" href="'
-				+ chrome.extension.getURL('css/trontastic/jquery-ui.css') + '">');
+				+ chrome.extension.getURL('css/trontastic/jquery-ui.css')
+				+ '">');
 
 $('#globalContainer').live('DOMSubtreeModified', function() {// DOMSubtreeModified
 
