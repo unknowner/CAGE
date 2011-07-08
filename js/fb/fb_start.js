8c7b5ffeb75c3f3788edce189b622bb60abd3658
CAGE <cageunknowner@googlemail.com>
2011-07-08 14:16:06 +0200

    CHG: Some cosmetics for the generals image

--- js/fb/fb_start.js
+++ js/fb/fb_start.js
@@ -1,12 +1,14 @@
 function startCAGE() {
 	$('div.fixedAux').css({
-		'width':245,
-		'border':0
-	}).empty().append(
-	$('<div id="cageGeneralContainer" class="ui-corner-br ui-state-default"></div>')
-	.append('<img id="cageGeneralImage" class="ui-corner-all" />')
-	.append('<span id="cageGeneralName" class="ui-state-active ui-corner-right"></span>')
-	).append('<div id="cageToolsContainer" class="ui-state-default ui-corner-right"></div>');
+		'width': 245,
+		'border': 0
+	}).empty()
+		.append(
+			$('<div id="cageGeneralContainer" class="ui-corner-br ui-state-default"></div>')
+				.append($('<div id="cageGeneralImageContainer" class="ui-state-active ui-corner-all"></div>')
+						.append('<img id="cageGeneralImage" class="ui-corner-all" />'))
+					.append('<span id="cageGeneralName" class="ui-state-active ui-corner-right"></span>'))
+		.append('<div id="cageToolsContainer" class="ui-state-default ui-corner-right"></div>');
 	console.log('init fb');
 	initTools();
 	com.send(com.task.signed, com.port.castleAge, $('input[name="signed_request"]').attr('value'));
