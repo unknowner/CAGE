fa583728e54c57012a32d8e1691c77d27afb07af
CAGE <cageunknowner@googlemail.com>
2011-07-10 21:10:15 +0200

    FIX: IFrame resizing FIX: Keep mod

--- js/castleage.js
+++ js/castleage.js
@@ -1,16 +1,29 @@
 // CAGE stuff working on Castle Age site
 var CastleAge = {
 	bqh: null,
-	signed_request: null
+	signed_request: null,
+	pageModTimer: null
 };
+
 com.initContentScript(com.port.castleAge);
 tools['Page'].runtime['allPages']();
+
 if (tools['Page'].runtime[$('#current_pg_info').attr('value') + '.php']) {
 	tools['Page'].runtime[$('#current_pg_info').attr('value') + '.php']();
 }
+
 $('head').append('<link rel="stylesheet" type="text/css" href="' + chrome.extension.getURL('css/trontastic/jquery-ui.css') + '">');
+
 initTools();
 
-addFunction(function(){FB.Canvas.setAutoResize(false);}, null, true, true);
+addFunction( function() {
+	FB.Canvas.setAutoResize(false);
+}, null, true, true);
+
+$('#app_body').live('DOMSubtreeModified', function(event) {
+	window.clearTimeout(CastleAge.pageModTimer);
+	CastleAge.pageModTimer = window.setTimeout(setCASize, 50);
+});
 
 com.send(com.task.castleAgeReady, com.port.facebook, {});
+setCASize();
--- js/page/keep.js
+++ js/page/keep.js
@@ -31,18 +31,26 @@ tools['Page'].runtime['keep.php'] = function () {
 			'width': 180
 		}).html('BSI: ' + _bsi.toFixed(2) + '<br><span style="font-size:9px;">Battle Strength Index</span><br>LSI: ' + _lsi.toFixed(2) + '<br><span style="font-size:9px;">Levelling Speed Index</span><br>TSI: ' + _tsi.toFixed(2) + '<br><span style="font-size:9px;">Total Skillpoints per Level</span><br>'));
 	}
-	
+
 	$('div.statsTTitle').toggle( function () {
-		$(this).parents('div.statsT2:first').animate({'height': '100%'}, 'fast');
-		console.log('pos:' + $(this).offset().top)
-		setCASize($(this).offset().top);
+		var _this = this;
+		$(this).parents('div.statsT2:first').animate({
+			'height': '100%'
+		}, 'fast', function() {
+			console.log('pos:' + $(this).offset().top)
+			setCASize($(_this).offset().top);
+		});
+		//console.log('pos:' + $(this).offset().top)
+		//setCASize($(this).offset().top);
 	}, function () {
-		$(this).parents('div.statsT2:first').animate({'height': 30}, 'fast');
-		setCASize();
+		$(this).parents('div.statsT2:first').animate({
+			'height': 30
+		}, 'fast', function() {
+			setCASize();
+		});
 	});
-	
 	$('div.statUnit a img').addClass('ui-corner-all');
-	
-	$('div.statUnit').css('height', 75).find('div:last:contains(X)').addClass('itemNumbers');
-	
+
+	$('div.statUnit').find('div:last:contains(X)').addClass('itemNumbers');
+
 };
