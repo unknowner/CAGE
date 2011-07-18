a1bc7f334bb36e517783791d02494730cc6c1206
CAGE <cageunknowner@googlemail.com>
2011-07-18 17:15:04 +0200

    FIX: Gifter finally working

--- js/tool/gifter.js
+++ js/tool/gifter.js
@@ -3,10 +3,10 @@ new tool('Gifter');
 tools['Gifter'].runtime = {};
 
 tools['Gifter'].update = function() {
-console.log('update');
-console.log(item.get('CAGEsendGiftTo'));
+	console.log('update');
+	console.log(item.get('CAGEsendGiftTo'));
 	tools['Gifter'].runtime['sendGiftTo'] = item.get('CAGEsendGiftTo', new Array());
-	if(tools['Gifter'].runtime['sendGiftTo'].indexOf(',') !== -1){
+	if(tools['Gifter'].runtime['sendGiftTo'].indexOf(',') !== -1) {
 		tools['Gifter'].runtime['sendGiftTo'] = tools['Gifter'].runtime['sendGiftTo'].split(',');
 	}
 	tools['Gifter'].runtime['requests'] = [];
@@ -28,7 +28,7 @@ console.log(item.get('CAGEsendGiftTo'));
 			console.log(tools['Gifter'].runtime['sendGiftTo']);
 			item.set('CAGEsendGiftTo', tools['Gifter'].runtime['sendGiftTo'].join(','));
 		}
-		com.send(com.task.fbButtonEnable, com.port.facebook, 'Gifter');
+		tools['Gifter'].work[com.port.castleAge]();
 	});
 	addFunction( function() {
 		FB.api('/me/apprequests/', function (_response) {
@@ -37,17 +37,117 @@ console.log(item.get('CAGEsendGiftTo'));
 	}, null, true, true);
 };
 tools['Gifter'].start[com.port.castleAge] = function () {
-	console.log('starting...');
-	console.log(tools['Gifter'].runtime['sendGiftTo']);
-	console.log(tools['Gifter'].runtime['requests']);
+	console.log('start[com.port.castleAge]');
+	tools['Gifter'].update();
+};
+tools['Gifter'].work[com.port.castleAge] = function () {
+	console.log('work[com.port.castleAge]');
+	if(tools['Gifter'].runtime['requests'].length > 0) {
+		console.log(tools['Gifter'].runtime['requests'].join(','))
+		$.get('index.php?request_ids=' + tools['Gifter'].runtime['requests'].join(',') + '&signed_request=' + $('#signed_request').val(), function(_data) {
+			console.log(_data);
+			tools['Gifter'].done[com.port.castleAge]();
+		});
+	} else {
+		tools['Gifter'].done[com.port.castleAge]();
+	}
+};
+tools['Gifter'].done[com.port.castleAge] = function () {
+	com.send(com.task.fbButtonEnable, com.port.facebook, 'Gifter');
 };
 tools['Gifter'].init[com.port.facebook] = function () {
 	tools['Gifter'].fbButton.add(chrome.i18n.getMessage("buttonGifter"), function () {
 		tools['Gifter'].fbButton.disable();
 		com.send(com.task.startGifter, com.port.castleAge, null);
 	});
-	tools['Gifter'].fbButton.disable();
-	com.send(com.task.updateGifter, com.port.castleAge);
+};
+tools['Gifter'].init[com.port.castleAge] = function () {
+	console.log('init gifter ca');
+	addFunction( function(_giftData) {
+
+		window['showRequestForm'] = function(tit, msg, track, request_params) {
+
+			console.log('CAGE showRequestForm');
+
+			var _ui = {
+				method: 'apprequests',
+				message: msg,
+				data: track,
+				title: tit
+			};
+
+			if(localStorage[FB._session.uid + '_' + 'CAGEsendGiftTo'] !== null ) {
+				_ui.filters = [{
+					name: 'CAGE',
+					user_ids: localStorage[FB._session.uid + '_' + 'CAGEsendGiftTo'].split(',')
+				},
+				'app_users',
+				'all',
+				'app_non_users'
+				];
+			}
+
+			FB.ui(_ui, function(result) {
+
+				// fixes infinite looping for popup window if u close it before it is done loading
+				$('.fb_dialog_iframe').each( function() {
+					$(this).remove();
+				});
+				if (result && result.request_ids) {
+
+					var request_id_string = String(result.request_ids);
+					var request_id_array = request_id_string.split(',');
+					var request_id_count = request_id_array.length;
+
+					// get all ids from sent gifts and remove them from the list
+					if(localStorage[FB._session.uid + '_' + 'CAGEsendGiftTo'] !== null) {
+						var ids = result.request_ids;
+						var _batch = [];
+						for (var i = 0; i < ids.length; i++) {
+							_batch.push({
+								"method": "get",
+								"relative_url": ids[i]
+							});
+						}
+						if (_batch.length > 0) {
+							FB.api('/', 'POST', {
+								batch: _batch
+							}, function (res) {
+								var _store = localStorage[FB._session.uid + '_' + 'CAGEsendGiftTo'].split(',');
+								for (var j = 0; j < res.length; j++) {
+									body = res[j].body;
+									var myObject = eval('(' + body + ')');
+									if(_store.indexOf(myObject.to.id) > -1) {
+										_store.splice(_store.indexOf(myObject.to.id),1);
+									}
+								}
+								localStorage[FB._session.uid + '_' + 'CAGEsendGiftTo'] = _store.join(',');
+								console.log('clear');
+								console.log(localStorage[FB._session.uid + '_' + 'CAGEsendGiftTo']);
+							});
+						}
+					}
+
+					var params = 'ajax=1';
+					params += '&signed_request=' + $('#signed_request').val();
+					$.ajax({
+						url: 'request_handler.php?'+request_params+'&request_ids='+result.request_ids,
+						context: document.body,
+						data: params,
+						type: 'POST',
+						success: function(data) {
+							var return_message = request_id_count + ' request' + (request_id_count == 1 ? '' : 's') + ' sent!';
+							document.getElementById('results_container').innerHTML = return_message;
+							FB.XFBML.parse(document.getElementById('results_container'));
+							$('#results_container').show();
+						}
+					}
+					);
+				}
+			}
+			);
+		}
+	}, null, true, true);
 };
 /*tools['Gifter'].xxxxx[com.port.castleAge] = function (_giftIds) {
 
