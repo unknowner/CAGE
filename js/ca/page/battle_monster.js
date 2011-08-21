4acc632594cb2a36e554e71fbc41c57d02351f45
CAGE <cageunknowner@googlemail.com>
2011-08-22 01:22:19 +0200

    FIX: % for Ragnarok armor

--- js/ca/page/battle_monster.js
+++ js/ca/page/battle_monster.js
@@ -11,13 +11,14 @@ tools['Page'].runtime['battle_monster.php'] = function() {
 		_monstername.text(_monstername.text() + ' (' + _percentage + (_percentage.indexOf('%') > -1 ? ')' : '%)'));
 	}
 
-	// add percentage to defense/forcefield/..
+	/// add percentage to defense/forcefield/..
 	var _defense = $('img[src*="bar_dispel.gif"],[src*="nm_green.jpg"],[src*="seamonster_ship_health.jpg"]').parent()[0];
-	var _defText = $('#app_body div:contains("Party Health/Strength"):last, #app_body div:textEquals("Skaar\'s Mana Forcefield "):first, #app_body div:textEquals("Illvasa, Plateau City\'s Defense "):first, #app_body div:textEquals("Castle Defense"):first, #app_body div:textEquals("Your Ship\'s Defense"):first');
+	var _defText = $('#app_body div:textEquals("Ragnarok\'s Glacial Armor "):first, div:contains("Party Health/Strength"):last, #app_body div:textEquals("Skaar\'s Mana Forcefield "):first, #app_body div:textEquals("Illvasa, Plateau City\'s Defense "):first, #app_body div:textEquals("Castle Defense"):first, #app_body div:textEquals("Your Ship\'s Defense"):first');
 	if(_defense && _defense.style && _defense.style.width !== "" && _defText && _defText.text()) {
 		var _percentage = _defense.style.width.substr(0, 5);
-		_defText.text(_defText.text() + ' (' + _percentage + (_percentage.indexOf('%') > -1 ? ')' : '%)'));
+		_defText.css('left', 51).text(_defText.text() + ' (' + _percentage + (_percentage.indexOf('%') > -1 ? ')' : '%)'));
 	}
+	
 	// fix for monster timer
 	addFunction(new Function($('#monsterTicker').next('script').html()), null, true, true);
 	addFunction(new Function($('#monsterTicker').parent().next('script').html()), null, true, true);
@@ -25,14 +26,14 @@ tools['Page'].runtime['battle_monster.php'] = function() {
 	// rearrange result
 	if($('span.result_body:contains("You attacked too soon and missed your attack")').length == 0) {
 		$('span.result_body:has(img[src*="graphics/button_monster_attack_again.gif"]) > div:first').append($('span.result_body img[src*="graphics/button_monster_attack_again.gif"]').parent().parent().css({
-			'position' : 'absolute',
-			'left' : 75,
-			'top' : 212
+			'position' : 'relative',
+			'top' : -94,
+			'left' : -244,
+			'marginBottom' : -82
 		}).detach()).find('img:first').css({
 			'height' : 142,
 			'borderRadius' : 6
 		});
-
 	}
 
 };
--- js/ca/page/festival_battle_monster.js
+++ js/ca/page/festival_battle_monster.js
@@ -11,12 +11,12 @@ tools['Page'].runtime['festival_battle_monster.php'] = function() {
 		_monstername.text(_monstername.text() + ' (' + _percentage + (_percentage.indexOf('%') > -1 ? ')' : '%)'));
 	}
 
-	// add percentage to defense/forcefield/..
+	/// add percentage to defense/forcefield/..
 	var _defense = $('img[src*="bar_dispel.gif"],[src*="nm_green.jpg"],[src*="seamonster_ship_health.jpg"]').parent()[0];
-	var _defText = $('#app_body div:contains("Party Health/Strength"):last, #app_body div:textEquals("Skaar\'s Mana Forcefield "):first, #app_body div:textEquals("Illvasa, Plateau City\'s Defense "):first, #app_body div:textEquals("Castle Defense"):first, #app_body div:textEquals("Your Ship\'s Defense"):first');
+	var _defText = $('#app_body div:textEquals("Ragnarok\'s Glacial Armor "):first, div:contains("Party Health/Strength"):last, #app_body div:textEquals("Skaar\'s Mana Forcefield "):first, #app_body div:textEquals("Illvasa, Plateau City\'s Defense "):first, #app_body div:textEquals("Castle Defense"):first, #app_body div:textEquals("Your Ship\'s Defense"):first');
 	if(_defense && _defense.style && _defense.style.width !== "" && _defText && _defText.text()) {
 		var _percentage = _defense.style.width.substr(0, 5);
-		_defText.text(_defText.text() + ' (' + _percentage + (_percentage.indexOf('%') > -1 ? ')' : '%)'));
+		_defText.css('left', 51).text(_defText.text() + ' (' + _percentage + (_percentage.indexOf('%') > -1 ? ')' : '%)'));
 	}
 
 	// fix for monster timer
@@ -25,16 +25,14 @@ tools['Page'].runtime['festival_battle_monster.php'] = function() {
 
 	// rearrange result
 	if($('span.result_body:contains("You attacked too soon and missed your attack")').length == 0) {
-		$('span.result_body:has(img[src*="graphics/button_monster_attack_again.gif"])').append($('span.result_body img[src*="graphics/button_monster_attack_again.gif"]').parent().parent().css({
-			'float' : 'right',
-			'marginRight' : 24
-		}).detach());
-		$('span.result_body:has(img[src*="graphics/button_monster_attack_again.gif"]) > div:eq(0) img:first').css({
-			'height' : 133
-		});
-		$('span.result_body:has(img[src*="graphics/button_monster_attack_again.gif"]) > div:eq(' + ($('span.result_body:contains("Elite Guard")').length == 0 ? 2 : 4) + ')').css({
-			'float' : 'left',
-			'width' : 550
+		$('span.result_body:has(img[src*="graphics/button_monster_attack_again.gif"]) > div:first').append($('span.result_body img[src*="graphics/button_monster_attack_again.gif"]').parent().parent().css({
+			'position' : 'relative',
+			'top' : -94,
+			'left' : -244,
+			'marginBottom' : -82
+		}).detach()).find('img:first').css({
+			'height' : 142,
+			'borderRadius' : 6
 		});
 	}
 
