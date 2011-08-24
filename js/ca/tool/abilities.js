9d719ae274a1790adc41d273e83b0bdaab3719a3
CAGE <cageunknowner@googlemail.com>
2011-08-24 02:16:59 +0200

    ADD: Class ability changer

--- js/ca/tool/abilities.js
+++ js/ca/tool/abilities.js
@@ -6,40 +6,145 @@ tools['Abilities'].runtime.general = null;
 
 tools['Abilities'].start = function() {
 
-};
-tools['Abilities'].equip_send = function(class_id) {
-	
-	friend_browse_offset = 0;
-	reset_raid_lst();
-	stopTimers = true;
+	get('guild_class.php', function(_data) {
 
-	var class_equips = classPowers[class_id];
-	if(class_equips.length == 0)
-		class_equips = false;
-	params = {}
-	params['ajax'] = 1;
-	params['class_id'] = class_id;
-	params['class_equips'] = class_equips;
-	params['signed_request'] = $('#signed_request').attr('value');
-	pageCache = {};
-	ajaxPerforming = true;
-	showLoaderIfAjax();
+		customEvent('AbilityChanged', function() {
+			tools['Abilities'].done();
+		});
 
-	$.ajax({
-		url : 'guild_class.php',
-		context : document.body,
-		data : params,
-		type : 'POST',
-		success : function(data) {
-			stopTimers = false;
-			ajaxPerforming = false;
-			$('#AjaxLoadIcon').hide();
-			$('#').slideUp('fast');
-			//$('#globalContainer').html(data);
-			//centerPopups();
-		}
+		$('#cageAbilitiesContainer').hide().html($('#expanded_power_cleric', _data).html());
+		addFunction(function() {
+
+			window['classPowers'] = {
+				'1' : [],
+				'2' : [],
+				'3' : [],
+				'4' : []
+			};
+
+			window['pickPower'] = function(class_id, cat, id, js_string_var) {
+
+				console.log(class_id + '-' + cat + '-' + id + '-' + js_string_var);
+				//var current_class_powers= classPowers[class_id];
+
+				//var max_class_slots = parseInt($('#class_slots_' + class_id).val());
+				console.log($('div[id^="free_slot_"]').length);
+				var max_class_slots = $('div[id^="free_slot_"]').length;
+				var class_power_elem = $('#class_power_' + class_id + '_' + cat + '_' + id);
+
+				if(class_power_elem && classPowers[class_id].length < max_class_slots) {
+
+					var big_img = $('#large_image_' + class_id + '_' + cat + '_' + id);
+					var gray_img = $('#gray_image_' + class_id + '_' + cat + '_' + id);
+
+					big_img.css('display', 'none');
+					gray_img.css('display', 'block');
+					class_power_elem.css('display', 'block');
+
+					classPowers[class_id].push({
+						'cat' : cat,
+						'id' : id
+					});
+
+					for(var i = 0; i < max_class_slots; i++) {
+						var freeSlotElem = $('#free_slot_' + class_id + '_' + i);
+						freeSlotElem.css('display', 'block');
+					}
+
+					for(var i = 0; i < classPowers[class_id].length; i++) {
+						var freeSlotElem = $('#free_slot_' + class_id + '_' + i);
+						freeSlotElem.css('display', 'none');
+					}
+
+				}
+			}
+			window['unpickPower'] = function(class_id, cat, id) {
+				console.log('unpickPower');
+				var class_power_elem = $('#class_power_' + class_id + '_' + cat + '_' + id);
+				//var max_class_slots = parseInt($('#class_slots_' + class_id).val());
+				console.log($('div[id^="free_slot_"]').length);
+				var max_class_slots = $('div[id^="free_slot_"]').length;
+				if(class_power_elem) {
+					var big_img = $('#large_image_' + class_id + '_' + cat + '_' + id);
+					var gray_img = $('#gray_image_' + class_id + '_' + cat + '_' + id);
+
+					big_img.css('display', 'block');
+					gray_img.css('display', 'none');
+					class_power_elem.css('display', 'none');
+
+					var counter = 0;
+					var newClassPowers = [];
+					for(var i = 0; i < classPowers[class_id].length; i++) {
+						var unpickedPower = classPowers[class_id][i];
+
+						if(unpickedPower['id'] != id || unpickedPower['cat'] != cat) {
+							newClassPowers.push(unpickedPower);
+						}
+
+					}
+					classPowers[class_id] = newClassPowers;
+
+					for(var i = 0; i < max_class_slots; i++) {
+						var freeSlotElem = $('#free_slot_' + class_id + '_' + i);
+						freeSlotElem.css('display', 'block');
+					}
+
+					for(var i = 0; i < classPowers[class_id].length; i++) {
+						var freeSlotElem = $('#free_slot_' + class_id + '_' + i);
+						freeSlotElem.css('display', 'none');
+					}
+
+				}
+			}
+			window['equip_send'] = function(class_id) {
+				console.log(class_id);
+				friend_browse_offset = 0;
+				reset_raid_lst();
+				stopTimers = true;
+
+				var class_equips = classPowers[class_id];
+				if(class_equips.length == 0)
+					class_equips = false;
+				params = {}
+				params['ajax'] = 1;
+				params['class_id'] = class_id;
+				params['class_equips'] = class_equips;
+				params['signed_request'] = $('#signed_request').attr('value');
+				console.log(params);
+				pageCache = {};
+				ajaxPerforming = true;
+				showLoaderIfAjax();
+
+				$.ajax({
+					url : 'guild_class.php',
+					context : document.body,
+					data : params,
+					type : 'POST',
+					success : function(data) {
+						stopTimers = false;
+						ajaxPerforming = false;
+						$('#AjaxLoadIcon').hide();
+						$('#cageAbilitiesContainer').slideUp('slow', function() {
+							$(this).empty();
+						});
+						fireAbilityChanged();
+
+					}
+				});
+			};
+		}, null, true, false);
+		addFunction(function() {
+			$('img[src*="class_cancelnew.jpg"]').parent().click(function() {
+				$('#cageAbilitiesContainer').slideUp('slow', function() {
+					$(this).empty();
+				});
+				fireAbilityChanged();
+			});
+		}, null, true, true);
+
+		$('div.imgButton[id^="large_image_"]').parent().css('cssText', 'float:left;padding:8px;');
+		$('#cageAbilitiesContainer').slideDown('slow');
 	});
-
 };
 
 tools['Abilities'].done = function() {
