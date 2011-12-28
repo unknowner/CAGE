1397706ed965ab6f26b56bdbda4f13e8f1c08959
CAGE <cageunknowner@googlemail.com>
2011-12-28 04:21:19 +0100

    1.1.4 ADD: Guild Class switcher

--- js/ca/tool/demi.js
+++ js/ca/tool/demi.js
@@ -6,9 +6,7 @@ tools.Demi.start = function() {
 	$('body > center').append('<div id="cageDemiResult">');
 	get('symbols.php', function(_demipage) {
 		$('div[id^="symbol_displaysymbols"]', _demipage).each(function(_i, _e) {
-			var _text = $(_e).text(),
-					_deity = /\+1 max (\w+)/.exec(_text)[1],
-					_points = /You have (\d+)/.exec(_text)[1]
+			var _text = $(_e).text(), _deity = /\+1 max (\w+)/.exec(_text)[1], _points = /You have (\d+)/.exec(_text)[1]
 			_demi.append('<div><div id="cageDemi' + _deity + '" class="cageDemiImage" style="background-image:url(http://image4.castleagegame.com/graphics/deity_' + _deity + '.jpg);" symbol="' + (_i + 1) + '"><span>' + _points + '<br>' + _deity.substr(0, 1).toUpperCase() + _deity.substr(1) + '</span></div></div>');
 		})
 		_demi.show().animate({
@@ -22,7 +20,7 @@ tools.Demi.start = function() {
 					resizable : false,
 					zIndex : 3999,
 					width : 440,
-					minHeight : 0, 
+					minHeight : 0,
 					position : ['center', 50],
 					draggable : false,
 					buttons : {
@@ -32,6 +30,17 @@ tools.Demi.start = function() {
 					},
 					autoOpen : true
 				});
+				if($('#cageDemiResult').text().indexOf('Your demi points for') > 0) {
+					var _wait = 24;
+					if(/Your demi points for (\w+)/.exec($('#cageDemiResult').text())[1] == 'Azeron') {
+						_wait = 48;
+					}
+					item.set('CAGEdemiLast', (new Date()).toISOString());
+					item.set('CAGEdemiNext', _wait);
+				} else {
+					console.log(/(\d+)(?= hours)/.exec($('#cageDemiResult').text()));
+					console.log(/(\d+)(?= minutes)/.exec($('#cageDemiResult').text()));
+				}
 			});
 			_demi.animate({
 				'top' : -100
--- js/castleage.js
+++ js/castleage.js
@@ -11,13 +11,13 @@ var CastleAge = {
 com.initPort(com.port.castleAge);
 
 $('head')
-	.append('<link id="cageTheme" rel="stylesheet" type="text/css" href="' + getPath('css/dark-hive/jquery-ui.css') + '?x=' + Math.random()*1000 + '">')
-	.append('<link rel="stylesheet" type="text/css" href="' + getPath('css/cage.css') + '?x=' + Math.random()*1000 + '">')
-	.append('<link rel="stylesheet" type="text/css" href="' + getPath('css/ca_cage.css') + '?x=' + Math.random()*1000 + '">')
-	.append('<link rel="stylesheet" type="text/css" href="' + getPath('css/ca_stats.css') + '?x=' + Math.random()*1000 + '">')
-	.append('<link rel="stylesheet" type="text/css" href="' + getPath('css/ca_general.css') + '?x=' + Math.random()*1000 + '">')
-	.append('<link rel="stylesheet" type="text/css" href="' + getPath('css/ui.selectmenu.css') + '?x=' + Math.random()*1000 + '">')
-	.append('<link rel="stylesheet" type="text/css" href="' + getPath('css/settings.css') + '?x=' + Math.random()*1000 + '">');
+	.append('<link id="cageTheme" rel="stylesheet" type="text/css" href="' + getPath('css/dark-hive/jquery-ui.css') + '?x=' + Math.random() + '">')
+	.append('<link rel="stylesheet" type="text/css" href="' + getPath('css/cage.css') + '?x=' + Math.random() + '">')
+	.append('<link rel="stylesheet" type="text/css" href="' + getPath('css/ca_cage.css') + '?x=' + Math.random() + '">')
+	.append('<link rel="stylesheet" type="text/css" href="' + getPath('css/ca_stats.css') + '?x=' + Math.random() + '">')
+	.append('<link rel="stylesheet" type="text/css" href="' + getPath('css/ca_general.css') + '?x=' + Math.random() + '">')
+	.append('<link rel="stylesheet" type="text/css" href="' + getPath('css/ui.selectmenu.css') + '?x=' + Math.random() + '">')
+	.append('<link rel="stylesheet" type="text/css" href="' + getPath('css/settings.css') + '?x=' + Math.random() + '">');
 	
 $('center:first').prepend('<div id="cageContainer"><div id="cageStatsContainer"></div><div id="cageToolsContainer" class="ui-widget-content ui-corner-bottom"></div></div>');
 
