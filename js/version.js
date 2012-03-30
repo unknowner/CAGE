828b997f537caee0ce318fdf7e1c6d8df0bb450f
unknown <David@.(none)>
2012-03-30 18:07:00 +0200

    1.2.0 init V1.2

--- js/version.js
+++ js/version.js
@@ -1,8 +1,8 @@
 // version stuff
 version = {
 	major : 1,
-	minor : 1,
-	revision : 32,
+	minor : 2,
+	revision : 0,
 	state : 'b',
 	string : function() {
 		return version.major + '.' + version.minor + '.' + version.revision + version.state;
