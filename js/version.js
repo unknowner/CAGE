dba3ad4f9403a94db8acb5f425103692cb4284d6
unknown <David@.(none)>
2012-03-30 17:58:01 +0200

    1.1.32 CHG: version var

--- js/version.js
+++ js/version.js
@@ -2,7 +2,7 @@
 version = {
 	major : 1,
 	minor : 1,
-	revision : 31,
+	revision : 32,
 	state : 'b',
 	string : function() {
 		return version.major + '.' + version.minor + '.' + version.revision + version.state;
