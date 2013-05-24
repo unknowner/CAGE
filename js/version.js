9db47eef25b357cc5c558069ab0006f93f84fc38
CAGE <cageunknowner@gmail.com>
2013-05-24 13:01:58 +0200

    VER: 1.3.0.0 Preparing new version

--- js/version.js
+++ js/version.js
@@ -1,9 +1,9 @@
 /** Current version, used for displaying and Firefox update check */
 version = {
 	major : 1,
-	minor : 2,
-	revision : 20,
-	build : 1,
+	minor : 3,
+	revision : 0,
+	build : 0,
 	state : 'b',
 	string : function() {
 		return version.major + '.' + version.minor + '.' + version.revision + '.' + version.build + version.state;
