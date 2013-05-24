b5b9e0e94a31d9ab735563a49994688f0c48def8
CAGE <cageunknowner@gmail.com>
2013-05-24 19:58:42 +0200

    VER: 1.3.0.0 FIX: Guild Filters (got/no health still buged)

--- js/ca/page/guild_battle.js
+++ js/ca/page/guild_battle.js
@@ -60,20 +60,18 @@ tools.Page.pages['guild_battle.php'] = function() {
 		var _count = 0;
 		$('#your_guild_member_list > div > div, #enemy_guild_member_list > div > div').each(function(_i, _e) {
 
-			var _text = $(_e).text().trim(), _stateTest = 1;
+			var _text = $(_e).text().trim(), _stateTest = true;
 			if (_state === "FullHealth") {
 				var _test = /(\d+)\/(\d+)/g.exec(_text);
-				_stateTest = (_test.length === 3 && _test[1] === _test[2]) ? 1 : 0;
+				_stateTest = (_test.length === 3 && _test[1] === _test[2]) ? true : false;
 			} else {
 				var _test = new RegExp(_state, "g");
-				console.log(_test.exec(_text));
-				_stateTest = _test.exec(_text) ? 1 : 0;
+				_stateTest = _test.test(_text);
 			}
 
 			var _classTest = _class === 'all' ? 1 : $(_e).find('img[src*="/graphics/class_' + _class + '.gif"]').length;
 			var _pointTest = _points === 'all' ? 1 : $(_e).find('img[title="Battle Points for Victory: ' + _points + '"]').length;
-			console.log(_classTest, _activ.test(_text), _pointTest, _stateTest);
-			if (_classTest > 0 && _activ.test(_text) && _pointTest > 0 && _stateTest > 0) {
+			if (_classTest > 0 && _activ.test(_text) && _pointTest > 0 && _stateTest === true) {
 				$(_e).show();
 				_count += 1;
 			} else {
